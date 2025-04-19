2353fe0cfd0c361860d0df4ec0b7c22533e37a2c
John Carlo Cabanilla <carlocabanilladev@outlook.com>
2025-04-19 12:00:17 -0700

    added directed and weighted graph functionality

--- graph.cpp
+++ graph.cpp
@@ -12,29 +12,102 @@ class Graph {
     public:
         vector<vector<int>> G;
         int size;
+        bool directed = false;
+        bool weighted = false;
+        vector<vector<vector<int>>> edgeWeight;
+        
 
         Graph(int n) {
             size = n;
             G.resize(n);
         }
 
+        Graph(int n,bool type) {
+            size = n;
+            G.resize(n);
+            directed = type;
+        }
+
+        Graph(int n,bool type,bool w) {
+            size = n;
+            G.resize(n);
+            weighted = w;
+            directed = type;
+            if (weighted) {
+                edgeWeight.resize(n);
+                for (int i = 0; i < n; ++i) {
+                    edgeWeight[i].resize(n); 
+                }
+            }
+            
+        }
+// Directed W/O Weight
         void AddEdge(int u, int v) {
             if (u >= size || v >= size || u < 0 || v < 0) {
                 throw std::out_of_range ("Invalid Node Index !!!");
             }
-            G[u].push_back(v);
-            G[v].push_back(u);
+            if (directed == true) {
+                G[u].push_back(v);
+            } else {
+                G[u].push_back(v);
+                G[v].push_back(u);
+            }
+         
+        }
+// Directed with Weight
+        void AddEdge(int u, int v,int w) {
+            if (u >= size || v >= size || u < 0 || v < 0) {
+                throw std::out_of_range ("Invalid Node Index !!!");
+            }
+            if (directed == true) {
+                G[u].push_back(v);
+                if (weighted)
+                    edgeWeight[u][v].push_back(w);
+            } else {
+                G[u].push_back(v);
+                G[v].push_back(u);
+                if (weighted) {
+                    edgeWeight[u][v].push_back(w);
+                    edgeWeight[v][u].push_back(w);
+                }
+          
+            }
+         
         }
 
         void Print(){
-            for (int i = 0; i < size; i ++) {
+            cout << "Graph Information" << endl;
+            
+            if (directed)
+                cout << "Type of Graph : Directed ";
+            else 
+                cout << "Type of Graph : Undirected ";
+                
+            if (weighted) {
+                cout << "Weigted Graph" << endl;
+
+                for (int i = 0; i < size; i ++) {
+                    cout << "Node " << i << ": ";
+    
+                    for (auto n : G[i]) {
+                        cout << "( " << n << " weight : " << edgeWeight[i][n][0] << ") ";
+                    }
+                    cout << endl;
+                }
+            }
+            else {
+                cout << "Unweigted Graph" << endl;
+
+                for (int i = 0; i < size; i ++) {
                 cout << "Node " << i << ": ";
 
                 for (auto n : G[i]) {
                     cout << n << " ";
                 }
                 cout << endl;
+                }
             }
+            
         }
 
         void BFS(int u) {
--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include "graph.cpp"
 
 int main() {
-    Graph G(5);
+    Graph G(5,true,true);
 
-    G.AddEdge(0, 1);
-    G.AddEdge(1, 2);
-    G.AddEdge(3, 2);
-    G.AddEdge(4, 1);
+    G.AddEdge(0, 1, 1);
+    G.AddEdge(1, 2, 3);
+    G.AddEdge(3, 2, 5);
+    G.AddEdge(4, 1, 6);
     // G.AddEdge(5,1);
 
     G.Print();
